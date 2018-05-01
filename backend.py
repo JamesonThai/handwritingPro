112fa158d6acb42361bb795d6d2a1e7713062a13
nityams <nityamshrestha@gmail.com>
2018-05-01 12:59:01 -0700

    u do it

--- backend.py
+++ backend.py
@@ -118,4 +118,4 @@ def prediction_page():
 if __name__ == '__main__':
 	# app.run(debug = True)
     port = int(os.environ.get('PORT',5000))
-    app.run(host='0.0.0.0', port = port)
+    app.run(host='0.0.0.0', port = port, debug = True)
--- templates/photo.js
+++ templates/photo.js
@@ -1,3 +1,21 @@
 (function(){
-  var video = document.getElementById('video');
+  var video = document.getElementById('video'),
+      vendorlUrl = window.URL || window.webkitURL;
+
+      navigator.getMedia = navigator.getUserMedia ||
+                            navigator.webkitGetUserMedia ||
+                            navigator.mozGetUserMedia ||
+                            navigator.msGetUserMedia;
+
+      navigator.getMedia({
+        video: true,
+        audio: false
+      }, function(stream){
+            video.src = vendorlUrl.createObjectUrl;
+            video.play();
+      }, function(error){
+        // An error occured
+        //error.code
+
+      });
 })();
